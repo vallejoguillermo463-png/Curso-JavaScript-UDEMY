eb8a63b1132973e53f66ed0c58905c18ee56bca8
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-05-22 22:29:13 -0500

    Ejercicio 1 Variales

---
+++ Primeros-pasos/variales.js
@@ -0,0 +1,14 @@
+var saludo = 'Hola Mundo'
+if (saludo){
+    var saludo = 'HOLA'
+    console.log(saludo)
+}
+console.log(saludo)
+let nombre = 'Guillermo'
+if (nombre){
+    let nombre = 'Segundo'
+    console.log(nombre)
+}
+console.log(nombre)
+const gravedad = '9.8'
+console.log(gravedad)
