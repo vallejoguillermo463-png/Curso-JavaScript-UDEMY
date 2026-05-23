cc954fcd85698976a58ec6a220fad9803982b36f
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-05-22 22:51:36 -0500

    Ejercicio 2 TiposV

---
+++ Primeros-pasos/tiposV.js
@@ -0,0 +1,19 @@
+var nombre = 'Guillermo'
+console.log(nombre)
+var miEdad = 56
+console.log(miEdad)
+const gravedad = 9.8
+console.log(gravedad)
+var verdadero = true
+var falso = false
+console.log(verdadero)
+console.log(falso)
+var nulo = null
+console.log(nulo)
+var indefinido = undefined
+console.log(indefinido)
+
+console.log(typeof 'Guillermo')
+
+const PI = Math.PI
+console.log(PI)
