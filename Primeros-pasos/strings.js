1f3651902f1076bc9d0219ab2ea98c68c360a55e
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-05-22 23:03:56 -0500

    Ejercicio 3 strings

---
+++ Primeros-pasos/strings.js
@@ -0,0 +1,11 @@
+var nombre = 'Guillermo'
+var edad = 56
+console.log('Mi nombre es '+nombre+' y tengo '+edad+' años')
+
+var nombreMayus = nombre.toUpperCase()
+
+console.log(nombreMayus)
+console.log(nombre.toLowerCase())
+console.log(nombre.length)
+
+
