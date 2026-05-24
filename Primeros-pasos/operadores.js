f56a3a67ea5a9bb7d48e7767b5dc64cc3969c9fb
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-05-24 17:46:10 -0500

    Ejercicio 4 operadores

---
+++ Primeros-pasos/operadores.js
@@ -0,0 +1,25 @@
+var a = 4 
+a = a + 6
+console.log(a)
+
+var a2 = 3
+console.log(a == a2)
+console.log(a >= a2)
+
+var b = 9
+b++
+console.log(b)
+
+var c = 4
+c--
+console.log(c)
+
+var v = true 
+var f = false 
+console.log(v && f)
+console.log(v || f)
+console.log(!v)
+
+var edad = 56
+var res = edad >= 18 ? console.log('mayor de edad') : console.log('menor de edad')
+console.log(res)
