e42b9c89d49b9688ed98793ae17d168d6dbc78b7
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-05-24 17:52:51 -0500

    Ejercicio 5 funciones

---
+++ Funciones/funciones.js
@@ -0,0 +1,16 @@
+function saludo(){
+    console.log('Hola a todos')
+}
+
+var miSuma = function (a,b,c){
+    return a + b + c
+}
+
+saludo()
+console.log(miSuma(2,8,4))
+console.log(miSuma(5,10,9))
+
+var a2 = 2
+var b2 = 5
+var c2 = 1
+console.log(miSuma(a2,b2,c2))
