3020ec575a07b226546b6b69a7e279569ebd3e49
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-05-25 21:58:13 -0500

    Ejercicio 11 if-else

---
+++ Condicionales/if-else.js
@@ -0,0 +1,23 @@
+var edad = 56
+
+if(edad >= 18 && edad <= 30){
+    console.log("Joven adulto")
+}else if(edad >=13 && edad < 18){
+    console.log("Adolescente")
+}else if(edad < 13){
+    console.log("Niño")
+}else if(edad > 30 && edad <= 65){
+    console.log("Adulto")
+}else if(edad > 65){
+    console.log("Anciano")
+}else{
+    console.log("Indefinido")
+}
+
+/*var variable = true
+
+if(variable){
+    console.log("Verdadero")
+}else{
+    console.log("Falso")
+}*/
