68fc7b4970d49ab99eef1a39a39087d169a2dfe6
Guillermo Vallejo <vallejoguillermo463@gmail.com>
2026-05-25 22:14:56 -0500

    Ejercicio 12 switch

---
+++ Condicionales/switch.js
@@ -0,0 +1,24 @@
+var opcion = "Distinto"
+
+switch (opcion) {
+    case 1:
+        console.log("Menu de usuario")
+        break;
+
+    case 2:
+        console.log("Menu administrativo")
+        break;
+
+    case 3:
+        console.log("Configuración")
+        break;
+
+    case "Nuevo menu":
+        let miNuevoMenu = "Salida"
+        console.log(miNuevoMenu)
+        break;
+
+    default:
+        console.log("Break")
+        break;
+}
